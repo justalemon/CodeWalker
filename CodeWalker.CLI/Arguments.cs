9ad7968f1ada9cc2b13de7f1bee028c6b78998b9
Hannele Ruiz <justlemoncl@gmail.com>
2021-11-26 09:09:25 -0300

    Added basic argument parsing

---
+++ CodeWalker.CLI/Arguments.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+
+namespace CodeWalker.CLI
+{
+    /// <summary>
+    /// The allowed Command Line Interface arguments.
+    /// </summary>
+    public class Arguments
+    {
+        /// <summary>
+        /// The source RPF file to modify.
+        /// </summary>
+        [Option('s', "source", HelpText = "The RPF File to use.", Required = true)]
+        public string Source { get; set; }
+
+        /// <summary>
+        /// Shows the info of the 
+        /// </summary>
+        [Option('i', "info", HelpText = "Show the information of the Source RPF and exit.", Group = "operations")]
+        public bool Operation { get; set; }
+    }
+}
--- CodeWalker.CLI/Program.cs
+++ CodeWalker.CLI/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace CodeWalker.CLI
+﻿using CommandLine;
+using System;
+
+namespace CodeWalker.CLI
 {
     /// <summary>
     /// The main entry point of the CodeWalker CLI.
@@ -6,12 +9,29 @@
     public static class Program
     {
         /// <summary>
+        /// The parsed CLI arguments.
+        /// </summary>
+        public static Arguments Arguments { get; set; }
+
+        /// <summary>
         /// The main entry point.
         /// </summary>
         /// <param name="args">The arguments passed to the executable.</param>
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
+            int exitCode = 0;
 
+            ParserResult<Arguments> arguments = Parser.Default.ParseArguments<Arguments>(args);
+
+            if (arguments.Tag == ParserResultType.NotParsed)
+            {
+                exitCode = 1;
+                return exitCode;
+            }
+
+            arguments.WithParsed(x => Arguments = x);
+
+            return exitCode;
         }
     }
 }
