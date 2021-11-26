f956dd626aa27e5648bf4e1003f5533af7fc62e2
Hannele Ruiz <justlemoncl@gmail.com>
2021-11-26 07:56:55 -0300

    Added basic CodeWalker Command Line Interface

---
+++ CodeWalker.CLI/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CodeWalker.CLI
+{
+    /// <summary>
+    /// The main entry point of the CodeWalker CLI.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point.
+        /// </summary>
+        /// <param name="args">The arguments passed to the executable.</param>
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
